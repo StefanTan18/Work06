8d3d21182b0f2789d8bee4c1d52689ff592e93f5
StefanTan18 <stan3@stuy.edu>
2018-10-02 11:11:39 -0400

    added mystruct.c

---
+++ mystruct.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "mystruct.h"
+
+
+
+struct sgrade{char s[8]; int g;};
+
+struct sgrade makestruct(){
+  srand( time(NULL) );
+  for (int i =)
+}
+
+void printstruct(struct sgrade s){
+}
+
+void changestruct(struct sgrade s, char *newname, int newgrd){
+}
+
