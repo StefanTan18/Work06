0567b413ef416cab2e21f5d6f92110f114366157
Mohammed Uddin <muddin3@stuy.edu>
2018-10-02 22:00:06 -0400

    add2struct

--- mystruct.c
+++ mystruct.c
@@ -6,14 +6,17 @@
 
 
 
-struct sgrade{char s[8]; int g;};
+struct sgrade{char str[8]; int g;};
 
 struct sgrade makestruct(){
   srand( time(NULL) );
-  for (int i =)
+  for (int i = 0; i < 8; i++){
+  	
+  }
 }
 
 void printstruct(struct sgrade s){
+	printf("%s has grade %d\n", s.str, s.g);
 }
 
 void changestruct(struct sgrade s, char *newname, int newgrd){
